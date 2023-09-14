6efd22a8bb67832219b64bb051123e82981e67c1
Bianca Ciorobea <ciorobea.b@northeastern.edu>
2023-09-14 15:27:00 -0400

    reverted last change

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,3 @@ const result_subtract = calc.subtract(3, 2);
 console.log(`result_add = ${result_add}`);
 console.log(`result_multiply = ${result_multiply}`);
 console.log(`result_subtract = ${result_subtract}`);
-
-console.log('done.');
